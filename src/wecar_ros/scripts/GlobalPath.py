7bfa5e485c0bc648bee8f0dda6cea167ff99fbd4
parkjbdev <parkjbdev@gmail.com>
2022-02-10 10:46:52 +0900

    Add Getter for Sliced GlobalPath

--- src/wecar_ros/scripts/GlobalPath.py
+++ src/wecar_ros/scripts/GlobalPath.py
@@ -1,5 +1,6 @@
 import sys
 from math import sqrt
+from typing import List
 
 import rospy
 from geometry_msgs.msg import PoseStamped
@@ -10,22 +11,26 @@ from Subscriber import VehicleStatus
 
 
 class GlobalPath:
-    __pathIndex = 0
+    __currentPathIndex = 0
 
-    def __init__(self, vehicleInfo: VehicleStatus, pathFileName):
-        self.__vehicleInfo = vehicleInfo
-        self.__load(pathFileName)
-
-    def getPath(self):
-        return self.__globalPath
-
-    def getPathIndex(self):
-        return self.__pathIndex
-
-    def __load(self, pathFileName):
+    def __init__(self, pathFileName, interestPointCount):
         self.__globalPath = Path()
         self.__globalPath.header.frame_id = "/map"
+        self.__load(pathFileName)
 
+    def getGlobalPath(self):
+        return self.__globalPath
+
+    def getSlicedGlobalPath(self, interestPointCount = 5):
+        lastPathIndex = min(
+            self.__currentPathIndex + interestPointCount, len(self.__globalPath)
+        )
+        return self.__globalPath.poses[self.__currentPathIndex:lastPathIndex]
+
+    def getCurrentPathIndex(self):
+        return self.__currentPathIndex
+
+    def __load(self, pathFileName):
         packageLocation = RosPack().get_path("wecar_ros")
         file = open(f"{packageLocation}/path/{pathFileName}.txt", "r")
 
@@ -46,33 +51,17 @@ class GlobalPath:
         vehiclePosition = self.__vehicleInfo.retrieve().get()
         currentDistance = self.__calcDistance(
             vehiclePosition,
-            self.__globalPath.poses[self.__pathIndex].pose.position,
+            self.__globalPath.poses[self.__currentPathIndex].pose.position,
         )
         nextDistance = self.__calcDistance(
             vehiclePosition,
-            self.__globalPath.poses[self.__pathIndex + 1].pose.position,
+            self.__globalPath.poses[self.__currentPathIndex + 1].pose.position,
         )
 
         if currentDistance > nextDistance:
-            self.__pathIndex += 1
+            self.__currentPathIndex += 1
 
-        return self.__pathIndex
+        return self.__currentPathIndex
 
     def __calcDistance(self, position1, position2):
         return sqrt((position1.x - position2.x) ** 2 + (position1.y - position2.y) ** 2)
-
-
-# class PathPlanner:
-#     def __init__(self, vehicleInfo: VehicleStatus, globalPathFileName):
-#         self.vehicleInfo = vehicleInfo
-#         self.globalPath = GlobalPath(vehicleInfo, globalPathFileName)
-
-#     # Returns Path in list
-#     def getPath(self):
-#         pass
-
-
-# if __name__ == "__main__":
-#     rospy.init_node("test", anonymous=True)
-#     globalPathFile = rospy.myargv(argv=sys.argv)[1]
-#     pathPlanner = PathPlanner(globalPathFile)
